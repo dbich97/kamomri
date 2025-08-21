edb425be5f641f8266d45d627aacb1ce21e27ad8
dbaich abdellah <dbich97@gmail.com>
2025-08-21 13:03:07 +0000

    Redirect root path to /fr

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,15 @@
-import createMiddleware from 'next-intl/middleware';
-import {locales, localePrefix, pathnames} from './navigation';
- 
-export default createMiddleware({
-  defaultLocale: 'ar',
-  locales,
-  localePrefix,
-  pathnames
-});
- 
-export const config = {
-  // Match only internationalized pathnames
-  matcher: [
-    // Enable a redirect to a matching locale at the root
-    '/',
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-    // Set a cookie to remember the previous locale for
-    // all requests that have a locale prefix
-    '/(ar|en|es|fr|de)/:path*',
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
 
-    // Enable redirects that add a locale prefix
-    // (e.g. `/about` -> `/en/about`)
-    '/((?!_next|_vercel|.*\\..*).*)'
-  ]
-};
+  // إذا زار المستخدم الرابط الرئيسي فقط "/"
+  if (pathname === '/') {
+    const url = request.nextUrl.clone();
+    url.pathname = '/fr'; // تغيير اللغة حسب ما تريده: /en أو /ar أو /fr
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
