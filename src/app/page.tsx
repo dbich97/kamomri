71a91cb36fb29631cdbce7c027ac8f28d89c07fc
dbich97 <dbich97@gmail.com>
2025-08-22 14:37:55 +0200

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation';
-import { locales }from '@/navigation';
-
-// This is the root page, it redirects to the default locale.
-// The actual homepage is in src/app/[locale]/page.tsx
-export default function RootPage() {
-  // The default locale is the first one in the array.
-  redirect(`/${locales[1]}`);
-}
